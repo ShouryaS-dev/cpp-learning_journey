67d256f078208c093a1cb4727ae02c55d2ca7537
ShouryaS-dev <shourya7sinha@gmail.com>
2026-06-13 22:21:46 +0530

    Add files via upload
    A collection of simple examples demonstrating for, while, and do-while loops in C++. 
    These examples show different ways to perform repetitive tasks and iterate through a sequence of values.

---
+++ loops.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+
+/*
+//basic for loop
+int main()
+{
+    for (int i=1; i<=5; i++)
+    cout<<i<<endl;
+}
+*/
+
+/*
+//basic while loop
+int main()
+{
+    int i = 1;
+    while (i <=5)
+    {
+        cout<<i<<endl;
+        i++;
+    }
+}
+*/
+
+/*
+//do while
+//only difference is that it ALWAYS runs atleast once
+int main()
+{
+    int i = 1;
+    do
+    {
+        cout<<i<<"\n";
+        i++;
+
+    }
+    while(i<=10);
+}
+*/
