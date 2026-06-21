a6d5995cf0e4900d9d250445a901c9ea912f8b69
ShouryaS-dev <shourya7sinha@gmail.com>
2026-06-21 16:14:51 +0530

    Add files via upload
    This programmes shows the execution of jump staatements (break, retur, and continue) in c++

---
+++ Loops_and_function/jumps_in_loops.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+//break only exits the loop...continues with the function
+//retrurn completely exits the function
+//continue just skips THAT iteration
+int breakOrReturn()
+{
+    while (true) // infinite loop
+    {
+        std::cout << "Enter 'b' to break or 'r' to return: ";
+        char ch{};
+        std::cin >> ch;
+
+        if (ch == 'b')
+            break; // execution will continue at the first statement beyond the loop
+
+        if (ch == 'r')
+            return 1; // return will cause the function to immediately return to the caller (in this case, main())
+    }
+
+    // breaking the loop causes execution to resume here
+
+    std::cout << "We broke out of the loop\n";
+
+    return 0;
+}
+
+int main()
+{
+    int returnValue{ breakOrReturn() };
+    std::cout << "Function breakOrReturn returned " << returnValue << '\n';
+
+    return 0;
+}
