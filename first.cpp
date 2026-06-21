3d9b1ecca8592a31f55c162cb32b8e8ac54cd085
ShouryaS-dev <shourya7sinha@gmail.com>
2026-06-21 15:39:54 +0530

    Add files via upload
    My first cpp programme

---
+++ first.cpp
@@ -0,0 +1,15 @@
+# include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int slices;
+    cout<< "How many slices: ";
+    cin>> slices;
+    float b = 1.5;
+    int a;
+    a = 2;
+    cout<< "You want " <<slices <<" slices of pizza " <<b;
+    return 0;
+}
